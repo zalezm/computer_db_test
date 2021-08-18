a8011467ea3b932a58e11657285537a779ffdd0c
Manuel Gonzalez <zalezm@gmail.com>
2021-08-18 08:48:28 -0700

    extend computers page and base page

--- computer_db_pom/common/base_page.py
+++ computer_db_pom/common/base_page.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from urllib.parse import urlparse
 
 
 class BasePage(object):
@@ -6,10 +8,12 @@ class BasePage(object):
         self.title = title
         self._web_driver = web_driver
         self.uri = uri
+        self._current_url = None
 
     def load_url(self, url: str):
         try:
             self._web_driver.get(url)
+            self._current_url = urlparse(self._web_driver.current_url)
         except Exception as e:
             print('Failed to load url {}: {}'.format(url, e))
             return False
@@ -19,5 +23,25 @@ class BasePage(object):
     def has_title(self):
         return hasattr(self._web_driver, 'title')
 
+    def get_title(self):
+        return self._web_driver.title if self.has_title() else None
+
     def has_expected_title(self):
-        return self.has_title() and self._web_driver.title == self.title
+        return self.has_title() and self.get_title() == self.title
+
+    def has_uri(self):
+        return self._current_url and self._current_url.path
+
+    def get_uri(self):
+        return self._current_url.path if self.has_uri() else None
+
+    def has_expected_uri(self):
+        return self.has_uri() and self.get_uri() == self.uri
+
+    def get_element_by_id(self, element_id: str):
+        try:
+            return self._web_driver.find_element_by_id(element_id)
+        except NoSuchElementException as e:
+            print('Element ID {} was not found on page: {}'.format(element_id, e))
+
+        return None
--- computer_db_pom/computers_page.py
+++ computer_db_pom/computers_page.py
@@ -4,6 +4,24 @@ from computer_db_pom.common.base_page import BasePage
 class ComputersPage(BasePage):
     TITLE = 'Computers database'
     URI = '/computers'
+    MAIN_CONTENT_ELEMENT_ID = 'main'
+    ACTIONS_HEADER_ELEMENT_ID = 'actions'
+    COMPUTERS_TABLE_CLASS_NAME = 'computers zebra-striped'
 
     def __init__(self, web_driver):
         super().__init__(web_driver, self.TITLE, self.URI)
+
+    def get_main_content(self):
+        return self.get_element_by_id(self.MAIN_CONTENT_ELEMENT_ID)
+
+    def has_main_content(self):
+        return bool(self.get_main_content())
+
+    def get_action_header(self):
+        return self.get_element_by_id(self.ACTIONS_HEADER_ELEMENT_ID)
+
+    def has_action_header(self):
+        return bool(self.get_action_header())
+
+    def has_page_content(self):
+        return self.has_main_content() and self.has_action_header()
