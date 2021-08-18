609aaa5f9e0246c6b681101e9760c5dc6bd79042
Manuel Gonzalez <zalezm@gmail.com>
2021-08-18 08:20:19 -0700

    add extra logging for step start and step end

--- features/environment.py
+++ features/environment.py
@@ -2,7 +2,16 @@ def before_scenario(context, scenario):
     context.web_driver = None
 
 
+def before_step(context, step):
+    print('------ Start Step ------')
+
+
+def after_step(context, step):
+    print('------ End Step ------')
+
+
 def after_scenario(context, scenario):
+    # cleanup web driver object after each scenario
     if context.web_driver:
         try:
             context.web_driver.close()
