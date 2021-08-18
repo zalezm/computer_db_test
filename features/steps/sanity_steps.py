8bb66be7dc5b403da4c7bbb1775cacff10e3765d
Manuel Gonzalez <zalezm@gmail.com>
2021-08-18 08:49:10 -0700

    use new pom functions to validate content

--- features/steps/sanity_steps.py
+++ features/steps/sanity_steps.py
@@ -39,7 +39,10 @@ def step_impl(context):
 
 @then(u'I should see the Computer Database home page')
 def step_impl(context):
+    assert context.current_page.has_expected_uri(), \
+        'Expecting URI {}, got {}'.format(context.current_page.uri, context.current_page.get_uri())
     assert context.current_page.has_expected_title(), \
-        'Expecting home page title {}, got {}'.format(ComputersPage.TITLE, context.web_driver.title)
+        'Expecting home page title {}, got {}'.format(context.current_page.title, context.current_page.get_title())
+    assert context.current_page.has_page_content(), 'Page content was not found!'
     # TODO: remove static sleep
     sleep(3)
